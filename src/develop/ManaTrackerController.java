59d50ee190a968890ef4cdcbd34b8dfa4c487534
Raidienaty <jcanducci@gmail.com>
2021-02-22 02:48:22 -0500

    added properites for each action

--- src/develop/ManaTrackerController.java
+++ src/develop/ManaTrackerController.java
@@ -4,7 +4,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +16,40 @@ public class ManaTrackerController extends AnchorPane
 {
     @FXML
     private Stage primaryStage;
+
+    @FXML
+    private AnchorPane currentManaPane;
+    @FXML
+    private Text currentMana;
+
+    @FXML
+    private AnchorPane resetManaPane;
+    @FXML
+    private Button resetManaButton;
+
+    @FXML
+    private AnchorPane modifyManaValuePane;
+    @FXML
+    private TextField changeMaxManaField;
+    @FXML
+    private Button changeMaxManaButton;
+    @FXML
+    private TextField addManaField;
+    @FXML
+    private Button addManaButton;
+    @FXML
+    private TextField removeManaField;
+    @FXML
+    private Button removeManaButton;
+
+    @FXML
+    private AnchorPane nameLoadPane;
+    @FXML
+    private Text name;
+    @FXML
+    private Button newButton;
+    @FXML
+    private Button loadButton;
 
     public Group initializeManaTracker(Stage primaryStage) throws IOException
     {
