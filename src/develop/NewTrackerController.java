9c53132b2e60882d67e90a46a1b85e480f31a068
Raidienaty <jcanducci@gmail.com>
2021-02-23 11:00:44 -0500

    Built syncing function for items on pane

--- src/develop/NewTrackerController.java
+++ src/develop/NewTrackerController.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,16 +18,39 @@ public class NewTrackerController
     @FXML
     private Stage primaryStage;
 
+    @FXML
+    private AnchorPane mainAnchorPane;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private Text errorMessage;
+    @FXML
+    private Button backButton;
+
     private List<SaveData> saveDataList;
 
     public Group initializeNewTracker(Stage primaryStage, List<SaveData> saveDataList) throws IOException
     {
         Parent root = loadFXML();
 
+        syncMainAnchor(root);
+
         setPrimaryStage(primaryStage);
         setSaveDataList(saveDataList);
 
         return new Group(root);
+    }
+    
+    private void syncMainAnchor(Parent root)
+    {
+        this.mainAnchorPane = (AnchorPane) root.getChildrenUnmodifiable().get(0);
+
+        this.nameField = (TextField) mainAnchorPane.getChildren().get(2);
+        this.submitButton = (Button) mainAnchorPane.getChildren().get(3);
+        this.errorMessage = (Text) mainAnchorPane.getChildren().get(4);
+        this.backButton = (Button) mainAnchorPane.getChildren().get(1);
     }
 
     private Parent loadFXML() throws IOException
